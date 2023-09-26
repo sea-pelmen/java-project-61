ac750a4ac026f9ee428f97f752203b1235d74e7e
Nastya <konopleva1995@gmail.com>
2023-09-27 00:16:21 +0300

    checkstyle changes

--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -10,23 +10,25 @@ public class Games {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         Scanner scanner = new Scanner(System.in);
 
-           for (var correctUsersAnswers = 0; correctUsersAnswers <= 2; correctUsersAnswers++) {
-               int evenGameNumber = (int) (Math.random() * 100);
-               System.out.println("Question: " + evenGameNumber);
-               System.out.print("Your answer: ");
+        for (var correctUsersAnswers = 0; correctUsersAnswers <= 2; correctUsersAnswers++) {
+            int evenGameNumber = (int) (Math.random() * 100);
+            System.out.println("Question: " + evenGameNumber);
+            System.out.print("Your answer: ");
 
-               var usersEvenGameAnswer = scanner.next();
+            var usersEvenGameAnswer = scanner.next();
             String correctAnswer = (evenGameNumber % 2 == 0) ? "yes" : "no";
             if (correctAnswer.equals(usersEvenGameAnswer)) {
                 System.out.println("Correct!");
             } else {
-                System.out.println("'" + usersEvenGameAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswer +"'.");
+                System.out.println("'" + usersEvenGameAnswer
+                        + "' is wrong answer ;(. Correct answer was '"
+                        + correctAnswer + "'.");
                 System.out.println("Let's try again, " + userName);
                 break;
             }
-               if (correctUsersAnswers == 2) {
-                   System.out.println("Congratulations, " + userName + "!");
-               }
+            if (correctUsersAnswers == 2) {
+                System.out.println("Congratulations, " + userName + "!");
+            }
         }
     }
 }
