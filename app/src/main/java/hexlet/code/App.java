51c24c7bcef85bff820356a69173d0cfa23cc605
Nastya <konopleva1995@gmail.com>
2023-10-02 01:40:26 +0300

    add ProgressionGame, update README by rec of ProgressionGame

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.EvenGame;
 import hexlet.code.games.CalcGame;
 import hexlet.code.games.GCDGame;
+import hexlet.code.games.ProgressionGame;
 
 import java.util.Scanner;
 
@@ -15,6 +16,7 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -25,6 +27,7 @@ public class App {
             case 2 -> EvenGame.even();
             case 3 -> CalcGame.calc();
             case 4 -> GCDGame.gcd();
+            case 5 -> ProgressionGame.progression();
             case 0 -> System.out.println("Bye!");
             default -> System.out.println("Invalid game number. Please restart the app.");
         }
---
+++ app/src/main/java/hexlet/code/games/ProgressionGame.java
@@ -0,0 +1,38 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Arrays;
+
+public class ProgressionGame {
+    public static void progression() {
+        String gameRules = "What number is missing in the progression?";
+
+        String[] questions = new String[3];
+        String[] answers = new String[3];
+
+        for (var correctUsersAnswers = 0; correctUsersAnswers <= 2; correctUsersAnswers++) {
+            int min = 5;
+            int max = 10;
+            int randomNum = min + (int) (Math.random() * ((max - min) + 1));
+            String[] progressionNumbers = new String[randomNum];
+            int randomQuestionIndex = (int) (Math.random() * progressionNumbers.length - 1);
+
+            progressionNumbers[0] = String.valueOf((int) (Math.random() * 100));
+
+            int randomD = (int) (Math.random() * 100);
+
+            for (int i = 1; i < progressionNumbers.length; i++) {
+                progressionNumbers[i] = String.valueOf((Integer.parseInt(progressionNumbers[i - 1]) + randomD));
+            }
+
+            String correctAnswer =
+                    String.valueOf(Integer.parseInt(progressionNumbers[randomQuestionIndex + 1]) - randomD);
+            progressionNumbers[randomQuestionIndex] = "..";
+            String progressionQuestion = Arrays.toString(progressionNumbers);
+            questions[correctUsersAnswers] = progressionQuestion;
+            answers[correctUsersAnswers] = correctAnswer;
+        }
+        Engine.gamesLogic(gameRules, questions, answers);
+    }
+}
